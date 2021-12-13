282c1da4db133af29bdc355c9dbb77baffa1d788
Marek Kuczynski <marek.kuczynski@stedi.com>
2021-12-13 23:42:05 +0100

    Added DynamoDB table, batch reading of SQS messages

--- lib/cdk-sqs-lambda-poller-stack.ts
+++ lib/cdk-sqs-lambda-poller-stack.ts
@@ -1,34 +1,50 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import { Tracing } from 'aws-cdk-lib/aws-lambda';
+import { Architecture, Tracing } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class CdkSqsLambdaPollerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const ddbTable = new Table(this, 'DdbTable', {
+      partitionKey: {
+        name: 'timest',
+        type: AttributeType.NUMBER,
+      },
+      sortKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+    
     const sqsQueue = new Queue(this, 'SqsQueue.fifo', {
-      visibilityTimeout: Duration.seconds(1),
+      visibilityTimeout: Duration.seconds(5),
       fifo: true,
+      contentBasedDeduplication: false,
       retentionPeriod: Duration.days(7)
     });
-
 
     const lambdaPollerFunction = new NodejsFunction(this, 'LambdaPollerFunction', {
       entry: './src/lambda-poller/index.ts',
       handler: 'handler',
       timeout: Duration.seconds(5),
-      memorySize: 128,
+      memorySize: 256,
       tracing: Tracing.ACTIVE,
+      architecture: Architecture.ARM_64,
       environment: {
-        QUEUE_URL: sqsQueue.queueUrl
+        QUEUE_URL: sqsQueue.queueUrl,
+        TABLE_NAME: ddbTable.tableName
       },
       deadLetterQueueEnabled: true
     });
 
+    ddbTable.grantReadWriteData(lambdaPollerFunction);
 
     const cronRule = new Rule(this, 'EventRule', {
       schedule: Schedule.rate(Duration.minutes(1)),
--- src/lambda-poller/index.ts
+++ src/lambda-poller/index.ts
@@ -1,19 +1,62 @@
-var awssdk = require('aws-sdk');
+const { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } = require("@aws-sdk/client-sqs");
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 
-var sqsclient = new awssdk.SQS({
+// Create SQS client
+var sqsclient = new SQSClient({
+    region: process.env.AWS_REGION
+});
+
+// Create DynamoDB client
+var ddbclient = new DynamoDBClient({
     region: process.env.AWS_REGION
 });
 
 exports.handler = async (event: any, context: any) => {
     
-    const msg = await sqsclient.receiveMessage({
+    // Get message from SQS
+    const sqsMsg = await sqsclient.send(new ReceiveMessageCommand({
         QueueUrl: process.env.QUEUE_URL,
-        MaxNumberOfMessages: 1,
-        VisibilityTimeout: 30,
+        AttributeNames: ['All'],
+        MaxNumberOfMessages: 10,
         WaitTimeSeconds: 0
-    }).promise();
+    }));
 
-    console.log(msg);
+    console.log(sqsMsg);
 
-    return msg;
-}
+    // Check if response contains messages
+    if (sqsMsg.Messages) {
+
+        console.log(sqsMsg.Messages);
+
+        for (const message of sqsMsg.Messages) {
+
+            // Get message body
+            console.log(message);
+
+            // Put message to DynamoDB
+            /*
+            const ddbData = await ddbclient.send(new PutItemCommand({
+                TableName: process.env.TABLE_NAME,
+                Item: {
+                    timest: 1, //Number(Date.now().toString().slice(0, -3)), 
+                    //id: message.MessageId,
+                    //message: message.Body,
+                    fullmsg: message
+                }
+            }));
+            */
+
+            // Delete message from SQS
+            const sqsData = await sqsclient.send(new DeleteMessageCommand({
+                QueueUrl: process.env.QUEUE_URL,
+                ReceiptHandle: message.ReceiptHandle
+            }));
+        };
+
+    } else {
+        console.log('No messages in queue');
+
+    };
+
+    return(sqsMsg.$metadata);
+};
